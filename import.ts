d57304096c88e6cb98e42ecf4f28bf6c2518a088
Felix Böhm <kontakt@felixboehm.it>
2025-02-05 15:34:15 +0100

    Remove debugs

--- import.ts
+++ import.ts
@@ -128,11 +128,3 @@ function calculateZoom(population: number): string {
   return "14";                             // Village/District
 }
 
-
-function hello(): void {
-  console.log('Hello Felix');
-}
-
-function test() {
-  return "test";
-}
